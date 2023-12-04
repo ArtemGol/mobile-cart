eef4898aad2693e550c93ebb4a15b3f53cf7dbbf
a.golovko <artiom.golovco@oneest.com>
2023-12-04 10:36:24 +0200

    add refresh control

--- src/screens/shopScreen/shopScreen.tsx
+++ src/screens/shopScreen/shopScreen.tsx
@@ -1,19 +1,23 @@
-import React, {FC, useState} from 'react';
-import {StyleSheet, FlatList} from 'react-native';
+import React, {FC, useCallback, useState} from 'react';
+import {StyleSheet, FlatList, RefreshControl} from 'react-native';
 import {BottomTabNavigationProp} from '@react-navigation/bottom-tabs';
 import {RootStackParamList} from '../../../assets/common/interfaces/rootBottomTabsTypes';
-import {useGetAllProductsQuery} from '../../api/productsApi';
+import {productsApi, useGetAllProductsQuery} from '../../api/productsApi';
 import {Loader} from '../../components/loader/loader';
 import {Product} from '../../components/product/product';
 import {NoDataComponent} from '../../components/noDataComponent/noDataComponent';
+import {colors} from '../../../assets/colors/colors';
+import {useAppDispatch} from '../../store';
 
 interface IProps {
   navigation: BottomTabNavigationProp<RootStackParamList, 'Shop'>;
 }
 
 const ShopScreen: FC<IProps> = () => {
+  const [refreshing, setRefreshing] = useState(false);
   const [page, setPage] = useState<number>(0);
   const {data, isLoading} = useGetAllProductsQuery(page);
+  const dispatch = useAppDispatch();
 
   const loadMoreItems = () => {
     //Обновление страницы нужно останавливать, когда пагинация закончилась
@@ -25,6 +29,14 @@ const ShopScreen: FC<IProps> = () => {
 
   //Лоудер нужно убирать, когда пагинация закончилась
   const footerComponent = data?.length ? () => <Loader /> : undefined;
+
+  const handleRefresh = useCallback(() => {
+    setRefreshing(true);
+    dispatch(productsApi.util?.resetApiState());
+    setPage(0);
+    setRefreshing(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <FlatList
@@ -38,6 +50,14 @@ const ShopScreen: FC<IProps> = () => {
       onEndReached={loadMoreItems}
       onEndReachedThreshold={0.7}
       removeClippedSubviews
+      refreshControl={
+        <RefreshControl
+          colors={[colors.blue]}
+          progressBackgroundColor={colors.white}
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
+        />
+      }
     />
   );
 };
